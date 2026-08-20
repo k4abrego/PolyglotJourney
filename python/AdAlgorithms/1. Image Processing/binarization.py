9ce32f4d25e032f2a3c1ed67d182814a1d689afa
k4abrego <188397826+k4abrego@users.noreply.github.com.>
2026-08-20 14:02:51 -0600

    Refactor code structure for improved readability and maintainability

--- python/AdAlgorithms/1. Image Processing/binarization.py
+++ python/AdAlgorithms/1. Image Processing/binarization.py
@@ -1,3 +1,29 @@
 from PIL import Image
 from typing import cast
-from rgb_types import 
+from rgb_types import RGBTuple, RGBStream
+
+threshold = 130
+#threshold = 100
+#threshold = 200
+
+def binarization(input_path: str, output_path: str) -> None:
+    in_img: Image.Image
+    with Image.open(input_path) as in_img:
+        in_img = in_img.convert('RGB')
+        in_stream: RGBStream = cast(RGBStream, in_img.get_flattened_data()) 
+        size: tuple[int, int] = in_img.size
+    out_stream: list[RGBTuple] = []
+    green: int
+    for (_, green, _) in in_stream:
+        out_stream.append(0 if green < threshold else 1) #the conditional statement below will determine if the pixel is black or white based on the threshold value
+        # if green < threshold:
+        #     out_stream.append(1)
+        # else:
+        #     out_stream.append(0)
+
+    out_img: Image.Iamge = Image.new('1', size)
+    out_img.putdata(out_stream)
+    out_img.save(output_path)
+
+if __name__ == '__main__':
+    binarization('images/woman.png', 'images/binarify_woman.png')
--- python/AdAlgorithms/1. Image Processing/gray_shades.py
+++ python/AdAlgorithms/1. Image Processing/gray_shades.py
@@ -1,3 +1,33 @@
 from PIL import Image
 from typing import cast
-from rgb_types import 
+from rgb_types import RGBTuple, RGBStream
+
+def grayify(input_path: str,
+            out_path1: str,
+            out_path2: str) -> None:
+    in_img: Image.Image
+    with Image.open(input_path) as in_img:
+        in_img = in_img.convert('RGB')
+        in_stream: RGBStream = cast(RGBStream, in_img.get_flattened_data()) 
+        size: tuple[int, int] = in_img.size
+    out_stream1: list[int] = [] #list to hold the pixel data for the output image
+    out_stream2: list[int] = [] #list to hold the pixel data for the output image
+    red: int
+    green: int
+    blue: int
+    for (red, green, blue) in in_stream:
+        average: int = (red + green + blue) // 3
+        out_stream1.append(average)
+        luma: int = int(0.299 * red + 0.587 * green + 0.114 * blue)
+        out_stream2.append(luma)
+
+    out_img1: Image.Image = Image.new('L', size) 
+    out_img1.putdata(out_stream1) 
+    out_img1.save(out_path1)
+
+    out_img2: Image.Image = Image.new('L', size) 
+    out_img2.putdata(out_stream2) 
+    out_img2.save(out_path2)
+
+if __name__ == '__main__':
+    grayify('images/snake.png', 'images/snake_gray_avg.png', 'images/snake_gray_luma.png')
