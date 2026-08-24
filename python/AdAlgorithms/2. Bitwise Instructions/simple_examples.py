71f44b8889580cf98b0bbe956566a1191cb1c4a3
k4abrego <188397826+k4abrego@users.noreply.github.com.>
2026-08-24 13:48:22 -0600

    Add simple examples for bitwise instructions in Python

---
+++ python/AdAlgorithms/2. Bitwise Instructions/simple_examples.py
@@ -0,0 +1,11 @@
+a: int = 5      # 0b0101
+b: int = 9      # 0b1001
+c: int = 15     # 0b1111
+
+print(f'{a = }')
+print(f'{b = }')
+print(f'{c = }')
+
+print(f'{bin(a) = }')
+print(f'{oct(b) = }')
+print(f'{hex(c) = }')
