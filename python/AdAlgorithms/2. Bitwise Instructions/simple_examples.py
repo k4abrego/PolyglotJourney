888a3da21d853dc48c0d4dea3c195db4b2a7484b
k4abrego <188397826+k4abrego@users.noreply.github.com.>
2026-08-24 14:29:33 -0600

    Add detailed examples for bitwise operations and conversions in Python

--- python/AdAlgorithms/2. Bitwise Instructions/simple_examples.py
+++ python/AdAlgorithms/2. Bitwise Instructions/simple_examples.py
@@ -9,3 +9,29 @@ print(f'{c = }')
 print(f'{bin(a) = }')
 print(f'{oct(b) = }')
 print(f'{hex(c) = }')
+
+s: str = '1010'
+print(f'{int(s) = }')
+print(f'{int(s, 2) = }')
+print(f'{int(s, 8) = }')
+print(f'{int(s, 10) = }')
+print(f'{int(s, 16) = }')
+print(f'{int(s, 36) = }') #36 is the maximum base allowed. 0-9 A-Z
+print(f'{int("zz", 36) = }') #35^z * 36^1 + 35^z * 36^0 = 1295
+
+print(f'{a & b = }') # bitwise AND
+print(f'{a & c = }')
+print(f'{b & c = }') 
+print()
+print(f'{a | b = }') # bitwise OR
+print(f'{a | c = }') 
+print(f'{b | c = }') 
+print()
+print(f'{a ^ b = }') # bitwise XOR
+print(f'{a ^ c = }') 
+print(f'{b ^ c = }')
+print()
+print(f'{~a = }') # bitwise NOT
+print(f'{~b = }')
+print(f'{~c = }')
+
